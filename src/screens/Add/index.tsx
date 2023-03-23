408d810b8ab34287147e2215347b9c1495cac1b1
Karen Grisales <karen.grisales@pragma.com.co>
2023-03-23 11:51:30 -0500

    Add screen add

--- src/screens/Add/index.tsx
+++ src/screens/Add/index.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, ImageBackground } from 'react-native';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { stylesGlobal } from '../../theme/theme';
+import { styles } from './styles';
 
 const Add = () => {
+  const [nameSpecie, setNameSpecie] = useState('');
+  const [locationSpecie, setLocationSpecie] = useState('');
+
   return (
-    <View>
-      <Text>Add</Text>
+    <View style={stylesGlobal.containerGlobal}>
+      <ImageBackground
+        source={require('../../assets/insects.png')}
+        style={styles.containerImage}>
+        <View style={styles.containerInputs}>
+          <Input
+            placeholder="Nombre de la especie"
+            value={nameSpecie}
+            onChangeInput={setNameSpecie}
+          />
+        </View>
+        <View style={styles.containerInputs}>
+          <Input
+            placeholder="Ubicación"
+            value={locationSpecie}
+            onChangeInput={setLocationSpecie}
+          />
+        </View>
+        <View style={styles.containerButton}>
+          <Button onPress={() => {}} name="Agregar" />
+        </View>
+      </ImageBackground>
     </View>
   );
 };
