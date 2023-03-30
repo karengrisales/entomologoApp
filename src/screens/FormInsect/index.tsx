8688316232bf3df29c052381b65acf6874e5f87a
Nicolas Ahumada <nicolas.ahumada@pragma.com.co>
2023-03-30 15:39:03 -0500

    feat: add form insect page

--- src/screens/FormInsect/index.tsx
+++ src/screens/FormInsect/index.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import ImageComponent from '../../components/Image';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import { stylesGlobal } from '../../theme/theme';
+import { styles } from './styles';
+
+const DATA = ['Hormiga', 'Abeja', 'Abejorro', 'Otro'];
 
 const FormInsect = () => {
-  return <Text>FormInsect</Text>;
+  const [insect, setInsect] = useState('');
+
+  return (
+    <View style={[stylesGlobal.containerGlobal, styles.container]}>
+      <View style={styles.image}>
+        <ImageComponent width={120} height={120} theme="imageCircleBorder" />
+      </View>
+      <View style={styles.inputs}>
+        <Select
+          label="Nombre especie"
+          placeholder="Seleccionar"
+          data={DATA}
+          onSelect={() => {}}
+        />
+        <Input
+          label="Información adicional"
+          placeholder="URL"
+          value={insect}
+          onChangeInput={setInsect}
+        />
+      </View>
+    </View>
+  );
 };
 
 export default FormInsect;
---
+++ src/screens/FormInsect/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    rowGap: 70,
+  },
+  image: {
+    alignItems: 'center',
+  },
+  inputs: {
+    flexDirection: 'column',
+    rowGap: 14,
+  },
+});
